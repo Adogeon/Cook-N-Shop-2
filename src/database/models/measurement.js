ecc69ea3aea141943bdf14ed721d843e85e5a137
Anh Dao <addao823@gmail.com>
2020-10-22 17:06:34 -0700

    adding test for databse models

--- src/database/models/measurement.js
+++ src/database/models/measurement.js
@@ -1,19 +1,5 @@
 module.exports = function (sequelize, DataTypes) {
   var Measurement = sequelize.define("Measurement", {
-    recipeID: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: Recipes,
-        key: "id",
-      },
-    },
-    ingredientID: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: Ingredients,
-        key: "id",
-      },
-    },
     quantity: {
       type: DataTypes.DECIMAL(10, 2),
     },
