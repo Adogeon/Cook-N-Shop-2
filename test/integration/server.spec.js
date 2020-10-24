2678a5f9a3f3990c30a7a230b114230989295199
Anh Dao <addao823@gmail.com>
2020-10-23 22:39:32 -0700

    installing sequelize-mock, prepare for testing resolver

---
+++ test/integration/server.spec.js
@@ -0,0 +1,9 @@
+//intended for server test
+
+//Steps:
+// - set up mocking sequelize database
+// - adding mock data
+// - send in graphql query for
+//   - query recipe
+//   - create new recipe
+//   - query ingredients
