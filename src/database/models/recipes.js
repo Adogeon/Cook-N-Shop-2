36a9ee5d19c0b1f3619be510cfbbfa639509127d
Anh Dao <addao823@gmail.com>
2020-10-26 11:50:41 -0700

    updating the graphql schema

--- src/database/models/recipes.js
+++ src/database/models/recipes.js
@@ -14,7 +14,7 @@ module.exports = function (sequelize, DataTypes) {
         len: [1],
       },
     },
-    instructions: {
+    instruction: {
       type: DataTypes.TEXT,
       allowNull: false,
       validate: {
