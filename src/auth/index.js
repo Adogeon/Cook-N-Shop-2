be358ec710308443a840cd828179def230bd8e56
Anh <addao823@gmail.com>
2021-01-23 15:58:49 -0800

    complete authRouter and authMiddleware

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
+const { User } = require("../database/models");
+const authRouter = express.Router();
 
 const getUser = (token) => {
   try {
@@ -12,10 +14,81 @@ const getUser = (token) => {
   }
 };
 
-const authorizeMiddleWare = (req, res, next) => {
+const authMiddleWare = (req, res, next) => {
   const token = req.get("Authorization") || "";
   req.user = getUser(token.replace("Bearer ", ""));
   next();
 };
 
+const registerUser = async ({ username, email, password }) => {
+  try {
+    const user = await User.create({
+      username,
+      email,
+      password: await bcrypt.hash(password, 10),
+    });
 
+    const token = jsonwebtoken.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1w" }
+    );
+
+    return {
+      token,
+      userId: user.id,
+    };
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const login = async ({ email, password }) => {
+  try {
+    const user = await User.findOne({ where: { email: email } });
+    if (!user) {
+      throw new Error("No user with that username");
+    }
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+      throw new Error("Incorrect password");
+    }
+
+    const token = jsonwebtoken.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1w" }
+    );
+    return {
+      token,
+      userId: user.id,
+    };
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+authRouter.get("/login", async (req, res) => {
+  try {
+    const token = await login(req.body);
+    res.json(token);
+  } catch (err) {
+    console.error(err);
+    res.send("Error" + err);
+  }
+});
+
+authRouter.get("/register", async (req, res) => {
+  try {
+    const token = await registerUser(req.body);
+    res.json(token);
+  } catch (err) {
+    console.error(err);
+    res.send("Error" + err);
+  }
+});
+
+module.exports = { authRouter, authMiddleWare };
--- src/graphql/subschema/User/resolver.js
+++ src/graphql/subschema/User/resolver.js
@@ -16,58 +16,6 @@ module.exports = {
       );
     },
   },
-  Mutation: {
-    registerUser: async (_, { username, email, password }) => {
-      try {
-        const user = await createNewUser({
-          username,
-          email,
-          password: await bcrypt.hash(password, 10),
-        });
-
-        const token = jsonwebtoken.sign(
-          { id: user.id, email: user.email },
-          process.env.JWT_SECRET,
-          { expiresIn: "1w" }
-        );
-
-        return {
-          token,
-          userId: user.id,
-        };
-      } catch (error) {
-        throw new Error(error.message);
-      }
-    },
-
-    login: async (_, { email, password }) => {
-      try {
-        const user = await User.findOne({ where: { email: email } });
-        if (!user) {
-          throw new Error("No user with that username");
-        }
-        const isValid = await bcrypt.compare(password, user.password);
-        if (!isValid) {
-          throw new Error("Incorrect password");
-        }
-
-        const token = jsonwebtoken.sign(
-          {
-            id: user.id,
-            email: user.email,
-          },
-          process.env.JWT_SECRET,
-          { expiresIn: "1w" }
-        );
-        return {
-          token,
-          userId: user.id,
-        };
-      } catch (error) {
-        throw new Error(error.message);
-      }
-    },
-  },
   Query: {
     currentUser: async (_, args, { currentUserId }) => {
       return await getUserById(currentUserId);
