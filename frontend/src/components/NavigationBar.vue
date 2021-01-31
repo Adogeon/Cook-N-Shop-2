2f23e2caef6ed8f00d8bf623d6657a3835a241f3
Anh <addao823@gmail.com>
2021-01-30 16:39:08 -0800

    Adding user profile page

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -1,31 +1,30 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">Cook-n-Shop</a>
+    <router-link class="navbar-brand" to="/">Cook-n-Shop</router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
       <ul class="navbar-nav me-2 mb-2 mb-lg-0">
         <li class="nav-item dropdown">
-          <a class="nav-link" href="/recipes">Recipes</a>
+          <router-link class="nav-link" to="/recipes">Recipes</router-link>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link" href="#">Search</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Profile
+            User
           </a>
           <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">User</a></li>
-            <li><a class="dropdown-item" href="#">Favorite Recipe</a></li>
+            <li><router-link class="dropdown-item" to="/user">Profile</router-link></li>
+            <li><router-link class="dropdown-item" to="/favorite">Favorite Recipe</router-link></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="nav-link" href="#" tabindex="-1">Log Out</a></li>
+            <li><router-link class="nav-link" to="/logout" tabindex="-1">Log Out</router-link></li>
           </ul>
         </li>
       </ul>
-     
     </div>
   </div>
 </nav>
